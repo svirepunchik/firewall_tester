4eb83268e96fb94662d412b389f7e80cd1840dbc
svirepunchik <svirepunchik@gmail.com>
2018-04-04 14:51:10 +0300

    refactored. issue #7

--- uninstall.sh
+++ uninstall.sh
@@ -7,14 +7,23 @@ iam=`whoami`
 
 if [[ $iam != "root" ]]; then echo "you must be root. please run sudo ./uninstall.sh"; exit 1; fi
 
-etcfld="/etc/firewall.tester"
-optfld="/opt/firewall.tester"
-usrbin="/usr/sbin/firewall.tester"
+etcfld="/etc/firewall-tester"
+usrbin="/usr/sbin/firewall-tester"
+hookfld="/usr/lib/firewall-tester"
+docfld="/usr/share/doc/firewall-tester"
+man1="/usr/share/man/man1/firewall-tester.1.gz"
+man1ru="/usr/share/man/ru/man1/firewall-tester.1.gz"
+cron="/etc/cron.daily/firewall-tester"
+lr="/etc/logrotate.d/firewall-tester"
 
-rm -rf $usrbin
-rm -rf $optfld
 rm -rf $etcfld
-
+rm -rf $usrbin
+rm -rf $hookfld
+rm -rf $docfld
+rm -rf $man1
+rm -rf $man1ru
+rm -rf $cron
+rm -rf $lr
 rm -rf /var/log/firewall.scan.log
 
 tmpcron=`mktemp`
