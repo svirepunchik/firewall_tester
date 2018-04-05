063e4228d28cb27e4a5dfecb17769436809eef15
svirepunchik <svirepunchik@gmail.com>
2018-04-05 14:19:27 +0300

    reorganized folders structure, updated man pages and readme

--- install.sh
+++ install.sh
@@ -10,16 +10,16 @@ if [[ $iam != "root" ]]; then echo "you must be root. please run sudo ./install.
 cp -rp ./etc /
 cp -rp ./usr /
 
-chmod -R a+x /usr/sbin/firewall-tester
-chmod -R a+x /usr/lib/firewall-tester/*.sh
-chmod -R a+r $etcfld
+chmod -R a+x /usr/bin/firewall-tester
+chmod -R a+x /usr/share/firewall-tester/*.sh
+chmod -R a+r /etc/firewall-tester
 
 mkdir -p /var/log
 touch /var/log/firewall.scan.log
 
 tmpcron=`mktemp`
 crontab -l > $tmpcron
-echo "0 22 * * * $usrbin -b" >> $tmpcron
+echo "0 22 * * * /usr/bin/firewall-tester -b" >> $tmpcron
 crontab $tmpcron
 rm -rf $tmpcron
 
